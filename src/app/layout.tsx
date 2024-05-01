048e9dd62b254c989a1de6f02b2fadbaeb38a24b
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-05-01 12:44:31 +0300

    create typography file; add font-weight values

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import '../styles/globals.css';
 
 const lato = Lato({
   subsets: ['latin'],
-  weight: "400"
+  weight: "300" || "400" || "500" || "700" || "800" || "900"
 });
 
 export const metadata: Metadata = {
