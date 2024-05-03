e513e1d1a65ead1d3840077aeb718a6cf8b9949d
OlegPanadiy <92667024+olehpanadii@users.noreply.github.com>
2024-05-03 21:28:57 +0100

    Revert "button component"
    This reverts commit 3867a5c55cd6abdfb1e8c778097d2d7ce8807a81.

--- src/components/UI/Buttons/Button.tsx
+++
@@ -1,8 +0,0 @@
-import Button from "@mui/material/Button";
-
-type PropsButtton = {
-  children?: React.ReactNode;
-  onClick?: () => void;
-  disabled?: boolean;
-  type: "button" | "submit" | "reset";
-};
