3d510e13d9e75258d5622eff8d1f8e01d0e528ee
OlegPanadiy <92667024+olehpanadii@users.noreply.github.com>
2024-05-04 23:32:18 +0100

    button update

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -15,7 +15,10 @@ type PropsButtton = {
     | "auth-sign-up-border"
     | "apply-btn-light"
     | "apply-btn-blue"
-    | "footer-book-now";
+    | "footer-book-now"
+    | "send"
+    | "deep-cleaning"
+    | "work-with-us";
 };
 
 export default function Button(props: PropsButtton) {
@@ -58,14 +61,28 @@ export default function Button(props: PropsButtton) {
       styles = " py-2 px-5 lg:px-6 bg-auth-button-bgn rounded-xl ";
       break;
     case "apply-btn-light":
-      styles = " rounded-xl  bg-secondary  py-1 px-[74px]  ";
+      styles =
+        " rounded-xl border-2 border-secondary   bg-secondary  py-1 px-[74px] hover:shadow-apply-send-button-shadow  focus:shadow-apply-send-button-shadow  ";
       break;
     case "apply-btn-blue":
-      styles = " rounded-xl  bg-main  py-1 px-[74px]  ";
+      styles =
+        " rounded-xl border-2 border-main  bg-main hover:border-2 hover:border-tertial  hover:bg-tertial/50 focus:bg-tertial/50 hover:shadow-apply-blue-button-shadow focus:shadow-apply-button-shadow  py-1 px-[74px]  ";
       break;
     case "footer-book-now":
       styles =
         " px-[14px] md:px-8 xl:px-12 py-1 border rounded-[10px] border-secondary bg-main";
+      break;
+    case "send":
+      styles =
+        " px-12 py-1 md:py-2 lg:px-16 lg:py-1 rounded-xl bg-secondary hover:shadow-apply-send-button-shadow  focus:shadow-apply-send-button-shadow ";
+      break;
+    case "deep-cleaning":
+      styles =
+        " px-5 py-3 border-2 rounded-xl border-tertial hover:border-main/30 focus:border-main/30 hover:bg-tertial focus:bg-tertial shadow-button-shadow hover:shadow-button-hover-shadow disabled:border-tertial/30 ";
+      break;
+    case "work-with-us":
+      styles =
+        " px-8 md:px-12 lg:px-[150px] xl:px-[424px] py-1 md:py-9 xl:py-14 rounded-xl bg-main ";
       break;
     default:
       break;
