153e38304648a3d9ade0fecd1328f7097dc308fa
RuslanManoilo <110188082+RuslanManoilo@users.noreply.github.com>
2024-05-05 13:51:26 +0300

    Added typography .body and colors from tailwind.config.ts

--- src/components/UI/Textarea.tsx
+++ src/components/UI/Textarea.tsx
@@ -8,7 +8,7 @@ type Props = {
 export default function Textarea(props: Props) {
   return (
     <textarea
-      className="w-full h-[199px] outline-none px-4 py-3 flex items-center gap-2 border-2 border-solid border-orange-300 rounded-md bg-white resize-none"
+      className="w-full h-full outline-none body px-4 py-3 flex items-center gap-2 border-2 border-solid border-secondary rounded-md bg-white resize-none"
       name={props.name}
       placeholder={props.placeholder}
       required={props.required}
