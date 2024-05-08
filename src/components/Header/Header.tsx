efc07d7b1f43037401b24c75ccf22dcaa8b66356
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2024-05-08 18:28:31 +0300

    added-responsive-to-logo-in-header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
 "use client";
-
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import ThemeSwitch from "./Switcher";
 import { useState } from "react";
 import { IconButton } from "@mui/material";
 import MenuIcon from "@mui/icons-material/menu";
 import { styled } from "@mui/material/styles";
+import Image from "next/legacy/image";
 
 const StyledMenuIcon = styled(MenuIcon)(() => ({
   color: "#006778",
@@ -18,6 +18,7 @@ const StyledMenuIcon = styled(MenuIcon)(() => ({
 
 export default function Header() {
   const [lightMode, setDarkMode] = useState(false);
+  const [auth] = useState(true);
 
   const handleChange = () => {
     setDarkMode(!lightMode);
@@ -25,19 +26,21 @@ export default function Header() {
 
   return (
     <header className="bg-white lg:bg-transparent">
-      <div className=" px-[20px] md:px-[28px] lg:px-[42px] xl:px-20 py-3 md:py-[18px] lg:py-5 flex direction-row justify-between lg:justify-end">
-        <div className="block lg:hidden">
+      <div className=" px-[20px] md:px-[28px] lg:px-[42px] xl:px-20 py-3 lg:py-[22px] flex direction-row justify-between lg:justify-end">
+        <div className="block lg:hidden w-[68px] h-[60px] md:w-[80px] md:h-[70px]">
           <Link href={"/"}>
             <Image
-              width={78}
-              height={66}
+              width={68}
+              height={60}              
               src="/icons/logo_shine.svg"
               alt="Logo"
+              layout="responsive"             
             />
           </Link>
         </div>
 
-        <div className=" flex direction-row gap-6 items-center justify-center ">
+        <div className=" flex direction-row gap-4 md:gap-[22px] lg:gap-10 items-center justify-center ">
+         {auth && <div className="text-accent lg:text-white subtext">Hello, Fiona</div>}
           <div className=" w-[38px] h-7 border rounded-[6px] border-light-main text-center">
             Uk
           </div>
--- src/components/Header/Switcher.tsx
+++ src/components/Header/Switcher.tsx
@@ -2,14 +2,14 @@ import Switch from "@mui/material/Switch";
 import { styled } from "@mui/material/styles";
 
 const ThemeSwitch = styled(Switch)(() => ({
-  width: 38,
-  height: 18,
+  width: 36,
+  height: 20,
   padding: 0,
   margin: 0,
   "& .MuiSwitch-switchBase": {
     padding: 1,
     "&.Mui-checked": {
-      transform: "translateX(20px)",
+      transform: "translateX(19px)",
       color: "#fff",
       "& + .MuiSwitch-track": {
         backgroundColor: "#006778",
@@ -29,7 +29,7 @@ const ThemeSwitch = styled(Switch)(() => ({
     backgroundColor: "rgba(0, 0, 0, 0.25)",
     opacity: 1,
   },
-  "@media (min-width: 768px)": {
+  "@media (min-width: 1440px)": {
     width: 44,
     height: 22,
     "& .MuiSwitch-switchBase": {
