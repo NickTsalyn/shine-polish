eb88a73ecf8234d57c6afc26cc761ab070960f7f
OlegPanadiy <92667024+olehpanadii@users.noreply.github.com>
2024-05-10 22:58:24 +0100

    hero section mark-up update

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -8,13 +8,13 @@ export default function Hero() {
   return (
     <>
       <section
-        className="p-5 lg:p-10 relative bg-cover bg-center bg-no-repeat mb-[60px] "
+        className="p-5 lg:p-10 xl:p-16 relative bg-cover bg-center bg-no-repeat mb-[60px] md:mb-[80px] lg:mb-[120px] xl:mb-[120px]"
         style={{
           backgroundImage: `linear-gradient(185deg, rgba(10, 10, 10, 0.39) 2.66%, rgba(120, 120, 120, 0.00) 81.3%), url(${mobileImage.src})`,
         }}
       >
         <div>
-          <h1 className=" h1 text-white text-shadow max-w-[282px] md:max-w-[730px] lg:max-w-[1124px] text-xl md:text-[52px] lg:text-[80px] md:leading-normal text-center font-medium mb-[138px] md:mb-[248px] lg:mb-[390px] mr-auto ml-auto">
+          <h1 className=" h1 text-white text-shadow max-w-[282px] md:max-w-[730px] lg:max-w-[1124px] xl:max-w-[1348px] text-xl md:text-[52px] lg:text-[80px] xl:text-8xl md:leading-normal text-center font-medium mb-[138px] md:mb-[248px] lg:mb-[390px] xl:mb-[600px] mr-auto ml-auto">
             Spend time on your loved ones, not on cleaning
           </h1>
 
