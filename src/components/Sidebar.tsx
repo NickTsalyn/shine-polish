2a8f3856cf594c03f2f568a1e10cb67d2a2b7bec
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2024-05-10 20:44:55 +0300

    added-buttons-&-socials

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,27 +1,46 @@
 "use client";
-
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import Image from "next/legacy/image";
 import Link from "next/link";
-import { styled } from "@mui/material/styles";
+import Image from "next/legacy/image";
 import { useState } from "react";
-import NavLinks from "./Navigation/NavLinks";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import WhatsAppIcon from "@mui/icons-material/WhatsApp";
+import { styled } from "@mui/material/styles";
 
-const StyledUserIcon = styled(AccountCircleIcon)(() => ({
+import NavLinks from "./Navigation/NavLinks";
+import Button from "./UI/Button";
+
+const UserIcon = styled(AccountCircleIcon)(() => ({
   color: "#fff",
   fontSize: 28,
 }));
+
+const socialIcons = [
+  {
+    icon: <FacebookRoundedIcon style={{ color: "white", fontSize: 36 }} />,
+    href: "https://facebook.com",
+  },
+  {
+    icon: <InstagramIcon style={{ color: "white", fontSize: 36 }} />,
+    href: "https://instagram.com",
+  },
+  {
+    icon: <WhatsAppIcon style={{ color: "white", fontSize: 36 }} />,
+    href: "https://whatsapp.com",
+  },
+];
 
 export default function Sidebar() {
   const [auth] = useState(true);
 
   return (
-    <aside className=" px-6 xl:px-[36px] pt-10 pb-6 hidden lg:block fixed inset-y-0 left-0 w-[200px] h-full xl:w-[244px] bg-main">
-      <div className="flex items-center justify-center w-[128px] h-[93px] xl:w-[156px] md:h-[106px] mx-auto mb-8 xl-[58px]">
+    <aside className="  hidden lg:flex flex-col content-around fixed inset-y-0 left-0 px-5 xl:px-[26px] pt-[46px] pb-6 w-[200px] h-full xl:w-[244px] bg-main">
+      <div className="flex items-center justify-center w-[128px] h-[92px] xl:w-[156px] md:h-[106px] mx-auto mb-9 xl:mb-[46px] ">
         <Link href={"/"}>
           <Image
             width={128}
-            height={93}
+            height={92}
             src="/icons/logo_dark_bg.svg"
             alt="Logo"
           />
@@ -29,13 +48,65 @@ export default function Sidebar() {
       </div>
 
       {auth && (
-        <div className="flex items-center gap-5 xl:gap-6 mb-8">
-          <StyledUserIcon />
-          <p className=" text-white body font-light">Hello, Fiona</p>
+        <div className="flex items-center gap-5 xl:gap-6 mb-6">
+          <UserIcon />
+          <p className=" body font-light text-white">Hello, Fiona</p>
         </div>
       )}
-      <NavLinks />
-      
+
+      <div className="mb-5 ">
+        <NavLinks />
+      </div>
+
+      <div className="mt-auto">
+        <ul className="mb-5 flex flex-col align-center gap-5 xl:gap-6">
+          {auth && (
+            <li>
+              <Button style="sidebar-log-out" type="button">
+                <span className="body text-secondary">Log Out</span>
+              </Button>
+            </li>
+          )}
+          {!auth && (
+            <li>
+              <Button style="sidebar-auth-in" type="button">
+                <span className="body text-secondary">
+                  Sign In <span className="text-white">or </span>Sign Up
+                </span>
+              </Button>
+            </li>
+          )}
+          <li>
+            <Button style="sidebar-book-now" type="button">
+              <span className="body font-bold text-secondary">Book Now</span>
+            </Button>
+          </li>
+          <li>
+            <div className="relative px-1 xl:px-[10px] py-1 border border-solid border-white rounded-xl text-[18px] xl:text-[20px] font-light xl:font-normal leading-normal text-white">
+              <Link href="tel:+4708003249">tel: 470-800-32-49</Link>
+
+              <div className="absolute top-[-18px] left-0 z-10">
+                <Image
+                  width={30}
+                  height={24}
+                  src="/images/woman_with_a_phone.png"
+                  alt="woman with a phone"
+                />
+              </div>
+            </div>
+          </li>
+        </ul>
+        <p className="mb-2 xl:mb-4 text-[28px] xl:text-[36px] font-light text-white">
+          Follow us
+        </p>
+        <ul className="flex  gap-5 ">
+          {socialIcons.map((socialIcon, index) => (
+            <li key={index}>
+              <Link href={socialIcon.href} >{socialIcon.icon} </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </aside>
   );
 }
--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -41,17 +41,17 @@ export default function Button(props: PropsButtton) {
       styles =
         "py-1 md:py-2 xl:py-3  px-3 md:px-16 lg:px-12 xl:px-17  border border-solid border-secondary rounded-[10px] bg-black bg-opacity-35 backdrop-blur-[10px]   ";
       break;
-    case "sidebar-book-now":
+    case "sidebar-book-now"://
       styles =
-        " px-8 xl:px-12 py-2 border rounded-[10px] border-secondary bg-main";
+        " px-8 xl:px-12 py-1 xl:py-[6px] w-full border rounded-[10px] border-secondary bg-main ";
       break;
-    case "sidebar-auth-in":
+    case "sidebar-auth-in"://
       styles =
-        " border border-solid border-sand rounded-[10px] px-[70px] xl:px-[22px] py-1 xl:py-2 bg-main";
+        " border border-solid border-sand rounded-[10px]  py-1 xl:py-[6px] bg-main w-full";
       break;
-    case "sidebar-log-out":
+    case "sidebar-log-out"://
       styles =
-        " border  rounded-[10px] border-sand bg-main px-10 xl:px-14 py-2";
+        " px-10 xl:px-14 py-1 xl:py-[6px] w-full border rounded-[10px] border-sand bg-main ";
       break;
     case "auth-sign-up-border":
       styles =
