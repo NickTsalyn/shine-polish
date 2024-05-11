502cbc5ca4f445d9e838c69f7ad24851cf9a4b53
FairySaikiNoKysyo <michaelevgeniymyers@gmail.com>
2024-05-11 22:03:40 +0300

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 import React from "react";
 
-const kaufmann = localFont({src: '../../public/fonts/kaufmann-bt.ttf'})
-
+const kaufmann = localFont({ src: "../../public/fonts/kaufmann-bt.ttf" });
 
 import { Reviews } from "@/components/Reviews";
 
 export default function Home() {
-  return <main>
-    <Reviews/>
-   <p className={`${kaufmann.className} text-lg`}>Very pleased with the quality of service provided by the cleaning company. Our home is sparkling clean, and the staff was very professional and attentive to detail.</p> 
-   
-  </main>;
+  return (
+    <main>
+      <Reviews />
+    </main>
+  );
 }
--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
-import localFont from 'next/font/local';
-const kaufmann = localFont({src: '../../public/fonts/kaufmann-bt.ttf'})
+// import localFont from 'next/font/local';
+// const kaufmann = localFont({src: '../../public/fonts/kaufmann-bt.ttf'})
 
 import React from "react";
 // Import Swiper styles
@@ -55,27 +55,34 @@ const reviews = [
 export const Reviews: React.FC = () => {
   return (
     <div className="container mx-auto sm:mb-[60px] md:mb-[80px] lg:mb-[120px] xl:mb-[160px]">
-      <h2 className="h2  text-center mb-[23px] text-main sm:text-[28px] md:text-[48px] lg:text-[64px] xl:text-[64px] sm:leading-34 md:leading-58 lg:leading-76 xl:leading-76 ">
+      <h2
+        className={` h2  text-center mb-[23px] text-main sm:text-[28px] md:text-[48px] lg:text-[64px] xl:text-[64px] sm:leading-34 md:leading-58 lg:leading-76 xl:leading-76 `}
+      >
         Reviews
       </h2>
       <div className="flex sm:gap-[53px] md:gap-[48px] lg:gap-20 xl:gap-20">
         <Swiper
-          slidesPerView={2}
+          slidesPerView={3}
           spaceBetween={30}
           freeMode={true}
           modules={[FreeMode]}
-          className="mySwiper  "
+          className={`mySwiper   `}
         >
           {reviews.map((review, index) => (
             <SwiperSlide
-              className="shadow-review-shadow border border-solid border-1  border-[#E6BA9540] sm:px-[12px] md:px-[28px] lg:px-5 xl:px-5 sm:py-[16px] md:py-[32px] lg:py-10 xl:py-10 relative"
+              className={` shadow-review-shadow border border-solid border-1  border-[#E6BA9540] sm:px-[12px] md:px-[28px] lg:px-5 xl:px-5 sm:py-[16px] md:py-[32px] lg:py-10 xl:py-10 relative`}
               key={index}
             >
               <div>
-                <p className={`${kaufmann.className}    text-center sm:text-[16px] md:text-[16px] lg:text-[24px] xl:text-[24px]  sm:leading-[19px] md:leading-[19px] lg:leading-[28px] xl:leading-[28px]  sm:mb-[9px] md:mb-[32px] lg:mb-[50px] xl:mb-[50px] `}>
-                  {review.review}
+                <p
+                  className={`    text-center sm:text-[16px] md:text-[16px] lg:text-[24px] xl:text-[24px]  sm:leading-[19px] md:leading-[19px] lg:leading-[28px] xl:leading-[28px]  sm:mb-[9px] md:mb-[32px] lg:mb-[50px] xl:mb-[50px] `}
+                >
+                  <span className="text-2xl"> {"❝"}</span> {review.review}
+                  {"❞"}
                 </p>
-                <p className={`${kaufmann.className}  sm:text-[16px] md:text-[16px] lg:text-[24px] xl:text-[24px] sm:leading-[19px] md:leading-[19px] lg:leading-[28px] xl:leading-[28px]  text-right max-h-[29px] absolute sm:bottom-[10px] md:bottom-[28px] lg:bottom-10 xl:bottom-10 sm:right-[12px] md:right-[28px] lg:right-5 xl:right-5`}>
+                <p
+                  className={`  sm:text-[16px] md:text-[16px] lg:text-[24px] xl:text-[24px] sm:leading-[19px] md:leading-[19px] lg:leading-[28px] xl:leading-[28px]  text-right max-h-[29px] absolute sm:bottom-[10px] md:bottom-[28px] lg:bottom-10 xl:bottom-10 sm:right-[12px] md:right-[28px] lg:right-5 xl:right-5`}
+                >
                   {review.author}
                 </p>
               </div>
