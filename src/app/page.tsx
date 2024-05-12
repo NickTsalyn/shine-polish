33950070228fbfc6e9f86f35fee43e909ab07ae6
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2024-05-13 00:32:39 +0300

    add images for slide, implement swiper in section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import localFont from 'next/font/local'
 
 const kaufmann = localFont({src: '../../public/fonts/kaufmann-bt.ttf'})
+import dataSlider from "@/data/slider-data.json";
 
 
 import BeforeAfter from "@/components/BeforeAfter";
@@ -8,7 +9,7 @@ import BeforeAfter from "@/components/BeforeAfter";
 export default function Home() {
   return <main>
     {/* <Footer/> */}
-    <BeforeAfter/>
+    <BeforeAfter data={dataSlider}/>
     {/* <p className={`${kaufmann.className} text-lg`}>Very pleased with the quality of service provided by the cleaning company. Our home is sparkling clean, and the staff was very professional and attentive to detail.</p> */}      {/* ЦЕ ДЛЯ ЖЕНІ ЗРАЗОК ЯК ПІДКЛЮЧИТИ КАСТОМНИЙ ШРИФТ*/}
   </main>;
 }
