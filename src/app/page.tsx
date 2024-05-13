1606328b918d1e44171924ed6c7a16225f5f0d8d
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-05-13 16:10:38 -0400

    fix images path

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import localFont from "next/font/local";
 const kaufmann = localFont({ src: "../../public/fonts/kaufmann-bt.ttf" });
 
 import { Reviews } from "@/components/Reviews";
+import LastSectionHome from "@/components/LastSectionHome";
 
 export default function Home() {
   return (
     <main>
       <Reviews />
+      <LastSectionHome/>
     </main>
   );
 }
