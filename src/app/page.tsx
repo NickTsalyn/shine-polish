eafb80a1454042afeeab7f7008df6c3848b63ada
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-05-16 21:40:40 -0400

    change svg icons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,12 @@ const kaufmann = localFont({ src: "../../public/fonts/kaufmann-bt.ttf" });
 
 import { Reviews } from "@/components/Reviews";
 import LastSectionHome from "@/components/LastSectionHome";
+import SectionEstimate from "@/components/SectionEstimate";
 
 export default function Home() {
   return (
     <main>
+      <SectionEstimate/>
       <Reviews />
       <LastSectionHome/>
     </main>
--- src/components/SectionEstimate.tsx
+++ src/components/SectionEstimate.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 import React from "react";
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
 import RemoveCircleOutlineOutlinedIcon from "@mui/icons-material/RemoveCircleOutlineOutlined";
+import minus from "../../public/icons/logo/minus.svg";
+import plus from "../../public/icons/logo/plus.svg";
 
 import img from "../images/estimate/estimate-mobile.png";
+
+const rooms = ["bedrooms", "bathrooms"];
 
 type Props = {};
 
@@ -14,39 +18,27 @@ const SectionEstimate = (props: Props) => {
 				<h2 className=" text-[30px] font-medium leading-[1.33] text-main text-center md:text-5xl mb-4 md:mb-11">
 					Estimate Calculation
 				</h2>
-				<div className="flex flex-col gap-6 justify-around items-center md:flex-row">
+				<div className="flex flex-col gap-6 justify-evenly items-center md:flex-row">
 					<div className=" flex flex-col gap-6 md:gap-10 lg:gap-12">
 						<ul className="flex flex-col gap-6 md:gap-12">
-							<li className="flex flex-row justify-around">
-								<p className=" font-normal text-base text-main flex justify-center items-center md:text-4xl md:leading-normal md:font-light">
-									bedrooms:
-								</p>
-								<div className="flex gap-3 items-center justify-center leading-normal">
-									<button>
-										<AddCircleOutlineOutlinedIcon sx={{ color: "#006778" }} className=" text-2xl md:text-[42px]" />
-									</button>
-									<span className=" font-medium text-base text-main md:text-4xl">1</span>
-									<button>
-										<RemoveCircleOutlineOutlinedIcon sx={{ color: "#006778" }} className=" text-2xl md:text-[42px]" />
-									</button>
-								</div>
-							</li>
-							<li className="flex flex-row justify-around">
-								<p className=" font-normal text-base text-main flex justify-center items-center md:text-4xl md:leading-normal md:font-light">
-									bathrooms:
-								</p>
-								<div className="flex gap-3 items-center justify-center leading-normal">
-									<button>
-										<AddCircleOutlineOutlinedIcon sx={{ color: "#006778" }} className=" text-2xl md:text-[42px]" />
-									</button>
-									<span className=" font-medium text-base text-main md:text-4xl">1</span>
-									<button>
-										<RemoveCircleOutlineOutlinedIcon sx={{ color: "#006778" }} className=" text-2xl md:text-[42px]" />
-									</button>
-								</div>
-							</li>
+							{rooms.map((room, index) => (
+								<li key={index} className="flex flex-row justify-around">
+									<p className=" font-normal text-base text-main flex justify-center items-center md:text-4xl md:leading-normal md:font-light">
+										{room}:
+									</p>
+									<div className="flex gap-7 items-center justify-center leading-normal">
+										<button>
+											<Image src={minus} alt="minus icon" className=" w-7 h-6 md:w-[42px] md:h-[42px]" />
+										</button>
+										<span className=" font-medium text-base text-main md:text-4xl">1</span>
+										<button>
+											<Image src={plus} alt="plus icon" className=" w-7 h-6 md:w-[42px] md:h-[42px]" />
+										</button>
+									</div>
+								</li>
+							))}
 						</ul>
-						<div className="flex flex-row justify-center items-center gap-2">
+						<div className="flex flex-row justify-between items-center gap-5">
 							<p className=" text-sand font-bold text-2xl md:font-normal md:text-[32px] lg:font-normal lg:text-5xl">
 								Previous estimate:
 							</p>
@@ -56,13 +48,11 @@ const SectionEstimate = (props: Props) => {
 						</div>
 					</div>
 					<button className=" max-w-[333px] h-[216px] rounded-xl bg-sand/25 flex justify-center items-center relative md:w-[226px] md:h-[243px] lg:w-[484px] lg:h-[338px]">
-						<span className="font-bold text-[44px] pr-20 pl-5 text-center text-accent md:text-[32px]">
-							Full estimate
-						</span>
+						<div className="font-bold text-[44px] pr-20 pl-5 text-center text-accent md:text-[32px]">Full estimate</div>
 						<Image
 							src={img}
 							alt="Estimate"
-							className=" absolute right-0 md:w-[122px] md:h-[243px] lg:w-[259px] lg:h-[480px]"
+							className=" absolute right-0 md:w-[122px] md:h-[243px] lg:w-[259px] lg:h-[400px]"
 						/>
 					</button>
 				</div>
