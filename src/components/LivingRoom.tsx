cdb28bc69ec288c3368388b57f259ca2011319f0
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2024-05-19 15:22:08 +0300

    added-responsive-img-and-icons-size

--- src/components/LivingRoom.tsx
+++ src/components/LivingRoom.tsx
@@ -1,16 +1,44 @@
 import Image from "next/legacy/image";
 import { LIVING_ROOM_SERVICES } from "@/global/living-room";
-import RoomIMG from "../../public/images/living-room@2x.webp";
-import IconPlus from "../../public/icons/Icon_plus.png";
+import IconPlus from "../../public/icons/icon_plus.png";
 import BasicBreadcrumbs from "./UI/Breadcrumbs";
+
 const icons = [
-  { top: "24%", left: "15%" },
-  { top: "11%", left: "30%" },
-  { top: "1%", right: "35%" },
-  { top: "28%", left: "30%" },
-  { bottom: "15%", right: "35%" },
-  { bottom: "1%", right: "10%" },
+  { top: "48%", left: "13%" },
+  { top: "24%", left: "36%" },
+  { top: "56%", left: "38%" },
+  { top: "3%", left: "54%" },
+  { top: "60%", left: "56%" },
+  { top: "50%", left: "68%" },
+  { top: "90%", left: "80%" },
 ];
+
+const RoomImage = () => {
+  return (
+    <picture>
+      <source
+        srcSet="/images/living-room/living-room.webp 1x, /images/living-room/living-room@2x.webp 2x"
+        type="image/webp"
+        sizes="(max-width: 375px) 278px, (max-width: 768px) 716px, (max-width: 1440px) 1076px"
+      />
+      <source
+        srcSet="/images/living-room/living-room1.jpg 1x, /images/living-room/living-room1@2x.jpg 2x"
+        type="image/jpeg"
+        sizes="(max-width: 375px) 278px, (max-width: 768px) 716px, (max-width: 1440px) 1076px"
+      />
+      <img
+        src="/images/living-room/living-room1.jpg"
+        alt="photo of living room"
+        style={{
+          width: "100%",
+          height: "auto",
+          objectFit: "cover",
+          objectPosition: "center",
+        }}
+      />
+    </picture>
+  );
+};
 
 export default function LivingRoomServices() {
   const { title, paragraph, processes, frequencies, needs } =
@@ -24,40 +52,14 @@ export default function LivingRoomServices() {
         roomName="Living Room"
       />
       <div className="relative mb-5 md:mb-10 lg:mb-[60px]">
-        <Image
-          src={RoomIMG}
-          alt="photo of living room"
-          width={278}
-          height={159}
-          layout="responsive"
-          objectFit="cover"
-          objectPosition="center"
-        />
-        {/* <div className="absolute top-[94px] left-[42px] z-20 flex items-center justify-center">
-        <Image  src={IconPlus} alt="icon plus" width={14} height={14} />
-      </div>
-      <div className="absolute top-[42px] left-[116px] z-20 flex items-center justify-center">
-        <Image src={IconPlus} alt="icon plus" width={14} height={14} />
-      </div>
-      <div className="absolute top-[1px] right-[140px] z-20 flex items-center justify-center">
-        <Image src={IconPlus} alt="icon plus" width={14} height={14} />
-      </div>
-      <div className="absolute top-[110px] left-[120px] z-20 flex items-center justify-center">
-        <Image src={IconPlus} alt="icon plus" width={14} height={14} />
-      </div>
-      <div className="absolute bottom-[60px] right-[130px] z-20 flex items-center justify-center">
-        <Image src={IconPlus} alt="icon plus" width={14} height={14} />
-      </div>
-      <div className="absolute bottom-[5px] right-[38px] z-20 flex items-center justify-center">
-        <Image src={IconPlus} alt="icon plus" width={14} height={14} />
-      </div> */}
+        <RoomImage />
         {icons.map((icon, index) => (
           <div
             key={index}
-            className="absolute z-20 flex items-center justify-center w-[14px] md:w-[37px] lg:w-[56px]"
-            style={{ ...icon, width: "14px", height: "14px" }}
+            className="absolute z-20  flex items-center justify-center size-5 md:size-9 lg:size-14"
+            style={{ top: icon.top, left: icon.left }}
           >
-            <Image src={IconPlus} alt="icon plus" width={14} height={14} />
+            <Image src={IconPlus} alt="icon plus" />
           </div>
         ))}
       </div>
