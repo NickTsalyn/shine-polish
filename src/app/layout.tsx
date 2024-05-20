700ad5aa4b585976938f0b9845b4e9a2e83e19cf
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-05-20 19:47:53 -0400

    fixing Footer bug part1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Lato } from "next/font/google";
 import "../styles/globals.css";
 import Header from "@/components/Header/Header";
 import Sidebar from "@/components/Sidebar";
-import Footer from '@/components/Footer';
 
 const lato = Lato({
   subsets: ["latin"],
@@ -26,7 +25,6 @@ export default function RootLayout({
         <Header />
         <Sidebar />
         <main className="lg:ml-[200px] xl:ml-[244px]">{children}</main>
-        <Footer />
       </body>
     </html>
   );
