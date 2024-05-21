8b88b73c9d342d9587f1bc7ee7f82f88d9422555
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-05-21 19:22:48 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import LastSectionHome from "@/components/LastSectionHome";
 import SectionEstimate from "@/components/SectionEstimate";
 import ServiceAreas from "@/components/ServiceAreas";
 import SectionJustAsk from "@/components/JustAskSection";
+import Hero from "@/components/hero";
 
 const kaufmann = localFont({ src: "../../public/fonts/kaufmann-bt.ttf" });
 
 export default function Home() {
   return (
     <>
+      <Hero/>
       <SectionEstimate />
       <Reviews />
       <ServiceAreas />
