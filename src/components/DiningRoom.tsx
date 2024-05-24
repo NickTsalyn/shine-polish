c5e15acffc3e5fe51d70faf14459dfb6c5d138c7
RuslanManoilo <110188082+RuslanManoilo@users.noreply.github.com>
2024-05-24 21:44:01 +0300

    renamed folder

--- src/components/DiningRoom.tsx
+++ src/components/DiningRoom.tsx
@@ -20,7 +20,7 @@ const CustomTooltip = (props: JSX.IntrinsicAttributes & TooltipProps) => (
     {...props}
     classes={{
       tooltip:
-        " bg-[#E6BA95CC] text-white text-center text-xs md:text-sm lg:text-base rounded-xl w-20 md:w-28 lg:w-36 lg:p-3",
+        " bg-[#E6BA95CC] text-white text-center text-xs md:text-sm lg:text-base rounded-xl w-20 md:w-28 lg:w-36 p-2 lg:p-3",
     }}
   />
 );
