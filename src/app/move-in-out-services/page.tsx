4dcbc59e0b8c0ebe1c950196eeb39c377a4ae69f
OlegPanadiy <92667024+olehpanadii@users.noreply.github.com>
2024-05-29 17:49:19 +0100

    Move-in-out-page

--- src/app/move-in-out-services/page.tsx
+++ src/app/move-in-out-services/page.tsx
@@ -6,7 +6,7 @@ export interface PageProps {}
 
 export default function Page({}: PageProps) {
   return (
-    <div className=" pt-5 px-[30px] pb-10 md:pt-10 md:px-10 md:pb-[60px] lg:p-10 lg:pb-20 xl:p-20">
+    <div className=" pt-5 px-[30px] pb-10 md:pt-10 md:px-10 md:pb-[60px] lg:p-[60px] lg:pb-[100px] xl:p-20">
       <MoveServices />
     </div>
   );
--- src/components/MoveInOutService.tsx
+++ src/components/MoveInOutService.tsx
@@ -12,17 +12,17 @@ export default function MoveServices() {
     aboutUs,
   } = MOVE_INOUT_SERVICES;
   return (
-    <div className=" body text-text flex flex-col justify-center md:justify-start items-center md:items-start ">
-      <h1 className=" subtitle-booking md:text-[32px] lg:text-6xl md:leading-6 text-main max-w-64 md:max-w-md lg:max-w-3xl text-center md:text-start mb-5 md:mb-10 lg:mb-7 ">
+    <div className=" body text-text  flex flex-col justify-center md:justify-start items-center md:items-start ">
+      <h1 className=" subtitle-booking md:text-5xl lg:text-6xl md:leading-6 text-main max-w-64 md:max-w-[682px] lg:max-w-[920px] text-center md:text-start mb-5 md:mb-10 lg:mb-7 ">
         {title}
       </h1>
       <div className=" mb-2 md:mb-5 lg:mb-0">
         <BasicBreadcrumbs
-          pageHref="appartment-services"
-          roomName="Professional Apartment Cleaning Service"
+          pageHref="move-in-out-services"
+          roomName="Move-In and Move-Out Cleaning"
         />
       </div>
-      <div className="max-w-64 md:max-w-3xl lg:max-w-6xl xl:max-w-screen-2xl">
+      <div className="max-w-64 md:max-w-[693px] lg:max-w-[1080px] xl:max-w-screen-2xl">
         <ul className="flex flex-col gap-5 lg:gap-8 mb-5 lg:mb-8">
           {paragraphs.map((p, index) => (
             <li key={index}>
@@ -30,7 +30,7 @@ export default function MoveServices() {
             </li>
           ))}
         </ul>
-        <h2 className=" text-xl text-main mb-5 lg:mb-8 ">
+        <h2 className=" text-3xl text-main mb-5 md:mb-0  ">
           {outServices.title}
         </h2>
         <ul className="flex flex-col gap-5 lg:gap-8 mb-5 lg:mb-8">
@@ -40,8 +40,65 @@ export default function MoveServices() {
             </li>
           ))}
         </ul>
-        <h2 className=" text-xl text-main mb-5 lg:mb-8 ">{inServices.title}</h2>
+        <h2 className=" text-3xl text-main mb-5 md:mb-0  ">
+          {inServices.title}
+        </h2>
         <p className="mb-5 lg:mb-8">{inServices.paragraphs}</p>
+        <h2 className=" text-3xl text-main ">{involves.title}</h2>
+        <p className="mb-5 lg:mb-8"> {involves.paragraph}</p>
+        <h3 className=" mb-5 font-bold lg:mb-8">{involves.smallTitle}</h3>
+        <h3 className=" font-bold">Kitchen</h3>
+        <p className=" mb-5 lg:mb-8">{involves.kitchen}</p>
+        <h3 className=" font-bold">General dusting</h3>
+        <ul className="flex flex-col gap-1 mb-5 md:mb-1 ">
+          {involves.generalDusting.map((p, index) => (
+            <li key={index}>
+              <p>{p}</p>
+            </li>
+          ))}
+        </ul>
+        <p className=" mb-5 lg:mb-8">{involves.generalDustingParagraph}</p>
+        <h3 className="mb-1 font-bold">General dusting</h3>
+        <ul className="flex flex-col gap-1 mb-5 md:mb-1 ">
+          {involves.generalDustingSecond.map((p, index) => (
+            <li key={index}>
+              <p>{p}</p>
+            </li>
+          ))}
+        </ul>
+
+        <p className=" mb-5 lg:mb-8">
+          {involves.generalDustingParagraphSecond}{" "}
+          <span className=" font-bold">
+            Ensuring these rooms are cleaned will allow you to relax and enjoy
+            your recreational activities best. We can help you with the
+            following:
+          </span>
+        </p>
+        <ul className="flex flex-col gap-1 mb-5 ">
+          {involves.generalDustingThird.map((p, index) => (
+            <li key={index}>
+              <p>{p}</p>
+            </li>
+          ))}
+        </ul>
+        <h2 className=" text-3xl text-main ">{benefits.titel}</h2>
+        <p className="mb-5">{benefits.paragraph}</p>
+        <h3 className="mb-5 font-bold">{benefits.subTitle}</h3>
+        <h3 className="mb-1 font-bold">{benefits.subTitleFirst}</h3>
+        <p className="mb-5">{benefits.paragraphFirst}</p>
+        <h3 className="mb-1 font-bold">{benefits.subTitleSecond}</h3>
+        <p className="mb-5">{benefits.paragraphSecond}</p>
+        <h3 className="mb-1 font-bold">{benefits.subTitleThird}</h3>
+        <p className="mb-5">{benefits.paragraphThird}</p>
+        <h2 className=" text-3xl text-main ">{aboutUs.title}</h2>
+        <ul className="flex flex-col gap-5  ">
+          {aboutUs.paragraps.map((p, index) => (
+            <li key={index}>
+              <p>{p}</p>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
