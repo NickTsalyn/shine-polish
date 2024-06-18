3dad455078d4725ef1637f2b043333ef2f16b592
KaterynaSch <128621831+KaterynaSch@users.noreply.github.com>
2024-06-18 13:36:06 +0300

    added-payment-link

--- src/components/Form/step_7.tsx
+++ src/components/Form/step_7.tsx
@@ -1,35 +1,20 @@
 "use client";
 
+import { Box } from "@mui/material";
 import Link from "next/link";
-import { useState } from "react";
-// import CheckoutForm from "../CheckoutForm";
-// import BasicModal from "../UI/Modal";
-// import { Elements } from "@stripe/react-stripe-js";
-// import { loadStripe } from "@stripe/stripe-js";
-
-// const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Step7 = () => {
-
-  // const [open, setOpen] = useState(false);
-
-  // const handleOpen = () => {
-  //     setOpen(true)
-  // }
-
-  // const handleClose = () => {
-  //     setOpen(false)
-  // }
   return (
     <div className="p-4 md:p-6 lg:p-9">
-      <h1>Little Form 7</h1>
-      <button type="button"  className="py-1.5 md:py-1 bg-accent text-white border border-solid border-accent rounded-xl w-[208px] h-[40px]">
-        <Link href="/payment">BOOK NOW</Link></button>
-      {/* <BasicModal open={open} onClose={handleClose}>
-      <Elements stripe={stripePromise}>
-        <CheckoutForm />
-        </Elements>
-      </BasicModal> */}
+      
+      <Box className=" flex justify-center items-center w-[208px] h-[40px]">
+      <Link
+        href="https://buy.stripe.com/test_3cs8AldjBeZj61G9AA"
+        className="py-1.5 md:py-1 bg-accent text-white border border-solid border-accent rounded-xl w-full text-center "
+      >
+        BOOK NOW
+      </Link>
+      </Box>
     </div>
   );
 };
