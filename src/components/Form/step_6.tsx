24a68eaaee89fad373c5a7282abf6c9484118962
OlegPanadiy <92667024+olehpanadii@users.noreply.github.com>
2024-06-22 17:20:02 +0100

    validation form

--- src/components/Form/step_6.tsx
+++ src/components/Form/step_6.tsx
@@ -1,9 +1,16 @@
 import useFormStorage from "@/hooks/formStorage";
 import Input from "../UI/Input";
-
-import { useState, ChangeEvent } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
+
+interface DataInput {
+  email: string;
+  phone: string;
+  name: string;
+  surname: string;
+}
 
 const Step6: React.FC = () => {
   const { form, handleInputChange, handleRadioChange } = useFormStorage({
@@ -15,14 +22,46 @@ const Step6: React.FC = () => {
   });
   const [remindersChecked, setRemindersChecked] = useState(false);
 
+  const {
+    register,
+    handleSubmit,
+    setValue,
+    formState: { errors },
+  } = useForm<DataInput>();
+
+  const onSubmit: SubmitHandler<DataInput> = (data) => {
+    console.log(data);
+    // Handle form submission
+  };
+
   const handleCheckBoxChange = () => {
     const updatedRemindersChecked = !remindersChecked;
     setRemindersChecked(updatedRemindersChecked);
     handleRadioChange("remindersChecked", updatedRemindersChecked);
   };
 
+  const handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue("email", event.target.value, { shouldValidate: true });
+    handleInputChange(event);
+  };
+
+  const handlePhoneChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue("phone", event.target.value, { shouldValidate: true });
+    handleInputChange(event);
+  };
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue("name", event.target.value, { shouldValidate: true });
+    handleInputChange(event);
+  };
+
+  const handleSurnameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue("surname", event.target.value, { shouldValidate: true });
+    handleInputChange(event);
+  };
+
   return (
-    <div className="py-4 md:py-6 lg:py-9">
+    <form onSubmit={handleSubmit(onSubmit)} className="py-4 md:py-6 lg:py-9">
       <h2 className="mb-4 md:mb-5 lg:mb-[26px] text-2xl md:text-4xl font-medium md:font-normal">
         Contact information
       </h2>
@@ -31,47 +70,83 @@ const Step6: React.FC = () => {
       </p>
       <div className="flex flex-col gap-2 md:gap-5 lg:gap-10 mb-4 md:mb-[350px] lg:mb-[90px]">
         <div className="flex flex-col lg:flex-row gap-2 md:gap-5 lg:gap-10">
-          <div className="md:w-[340px] lg:w-[523px]">
+          <div className="md:w-[340px] lg:w-[523px] relative">
             <Input
+              {...register("name", { required: "First Name is required" })}
               name="name"
-              value={form.name as string}
-              onChange={handleInputChange}
+              value={form.name}
+              onChange={handleNameChange}
               type="text"
               placeholder="First Name*"
               style="form-input"
             />
+            {errors.name && (
+              <p className="text-xs absolute bottom-[-8px] left-2">
+                {errors.name.message}
+              </p>
+            )}
           </div>
-          <div className="md:w-[340px] lg:w-[563px]">
+          <div className="md:w-[340px] lg:w-[563px] relative">
             <Input
+              {...register("surname", { required: "Last Name is required" })}
               name="surname"
-              value={form.surname as string}
-              onChange={handleInputChange}
+              value={form.surname}
+              onChange={handleSurnameChange}
               type="text"
               placeholder="Last Name*"
               style="form-input"
             />
+            {errors.surname && (
+              <p className="text-xs absolute bottom-[-8px] left-2">
+                {errors.surname.message}
+              </p>
+            )}
           </div>
         </div>
         <div className="flex flex-col lg:flex-col-reverse gap-2 md:gap-5 lg:gap-10">
-          <div className="lg:w-[700px] lg:h-[57.5px]">
+          <div className="lg:w-[700px] lg:h-[57.5px] relative">
             <Input
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                  message: "Invalid email address",
+                },
+              })}
               name="email"
-              value={form.email as string}
-              onChange={handleInputChange}
+              value={form.email}
+              onChange={handleEmailChange}
               type="email"
               placeholder="Email*"
               style="form-input"
             />
+            {errors.email && (
+              <p className="text-xs absolute bottom-[-8px] left-2">
+                {errors.email.message}
+              </p>
+            )}
           </div>
-          <div className="md:w-[515px] lg:w-[390px] lg:h-[57.5px]">
+          <div className="md:w-[515px] lg:w-[390px] lg:h-[57.5px] relative">
             <Input
+              {...register("phone", {
+                required: "Phone number is required",
+                pattern: {
+                  value: /^\(\d{3}\)\d{3}-\d{4}$/,
+                  message: "Phone number must be in the format (999)999-9999",
+                },
+              })}
               name="phone"
-              value={form.email as string}
-              onChange={handleInputChange}
+              value={form.phone}
+              onChange={handlePhoneChange}
               type="tel"
               placeholder="Phone*"
               style="form-input"
             />
+            {errors.phone && (
+              <p className="text-xs absolute bottom-[-8px] left-2">
+                {errors.phone.message}
+              </p>
+            )}
           </div>
         </div>
       </div>
@@ -91,7 +166,7 @@ const Step6: React.FC = () => {
           Send me reminders about my booking via email
         </p>
       </div>
-    </div>
+    </form>
   );
 };
 
