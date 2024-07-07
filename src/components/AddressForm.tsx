c261752a3dc89987f3307b6b8947c5a5ab4fca5f
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-07-07 16:44:48 -0400

    Update AddressForm.tsx

--- src/components/AddressForm.tsx
+++ src/components/AddressForm.tsx
@@ -3,120 +3,127 @@ import Autocomplete from "react-google-autocomplete";
 import Input from "../components/UI/Input";
 import useFormStorage from "@/hooks/formStorage";
 import { useForm } from "react-hook-form";
+import Button from "./UI/Button";
 
 // const API_KEY = process.env.GOOGLE_MAPS_API_KEY as string;
 const API_KEY = "AIzaSyCjw4zjS8V3L0IwDnqWwWz5bXh6w9b4Hc8";
 
 type FormValues = {
-  address: string;
-  aptSuite: string;
-  city: string;
-  zipCode: string;
-  selectedDate: string;
-  time: string;
+	address: string;
+	aptSuite: string;
+	city: string;
+	zipCode: string;
+	selectedDate: string;
+	time: string;
 };
 
 const AddressForm: React.FC = () => {
-  const { form, handleCustomChange } = useFormStorage({
-    address: "",
-    aptSuite: "",
-    state: "",
-    city: "",
-    zipCode: "",
-    selectedDate: "",
-    time: "",
-  })
+	const { form, handleCustomChange } = useFormStorage({
+		address: "",
+		aptSuite: "",
+		state: "",
+		city: "",
+		zipCode: "",
+		selectedDate: "",
+		time: "",
+	});
 
-  const formData = useForm<FormValues>();
-  const [inputValue, setInputValue] = useState('');
-  const [addressDetails, setAddressDetails] = useState({
-    street: form.address,
-    city: form.city,
-    state: form.state,
-    zip: form.zipCode
-  });
-  
-  console.log(addressDetails)
+	const formData = useForm<FormValues>();
+	const [inputValue, setInputValue] = useState("");
+	const [addressDetails, setAddressDetails] = useState({
+		street: form.address,
+		city: form.city,
+		state: form.state,
+		zip: form.zipCode,
+	});
 
-  const {
-    register,
-    formState: { errors },
-  } = formData;
+	console.log(addressDetails);
 
-  const handlePlaceSelected = (place: google.maps.places.PlaceResult) => {
-    if (!place || !place.address_components) {
-      return;
-    }
+	const {
+		register,
+		formState: { errors },
+	} = formData;
 
-    const addressComponents = place.address_components;
-    let streetNumber = '';
-    let streetName = '';
-    let city = '';
-    let state = '';
-    let zip = '';
+	const handlePlaceSelected = (place: google.maps.places.PlaceResult) => {
+		if (!place || !place.address_components) {
+			return;
+		}
 
-    const fullAddress = place.formatted_address;
-    
+		const addressComponents = place.address_components;
+		let streetNumber = "";
+		let streetName = "";
+		let city = "";
+		let state = "";
+		let zip = "";
 
-    addressComponents.forEach(component => {
-      const types = component.types;
-      if (types.includes('street_number')) {
-        streetNumber = component.long_name;
-      }
-      if (types.includes('route')) {
-        streetName = component.long_name;
-      }
-      if (types.includes('locality')) {
-        city = component.long_name;
-      }
-      if (types.includes('administrative_area_level_1')) {
-        state = component.short_name;
-      }
-      if (types.includes('postal_code')) {
-        zip = component.long_name;
-      }
-  });
-  const street = `${streetNumber} ${streetName}`.trim();
-  setAddressDetails({ street, city, state, zip });
-  setInputValue(fullAddress ?? ''); // Set the input value to the full street address
-}
+		const fullAddress = place.formatted_address;
 
-const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-  setInputValue(e.target.value);
-  // setAddressDetails({ ...addressDetails, street: e.target.value });
-  handleCustomChange("address", addressDetails);
-  setAddressDetails({ ...addressDetails, street: inputValue });
-  
-};
+		addressComponents.forEach((component) => {
+			const types = component.types;
+			if (types.includes("street_number")) {
+				streetNumber = component.long_name;
+			}
+			if (types.includes("route")) {
+				streetName = component.long_name;
+			}
+			if (types.includes("locality")) {
+				city = component.long_name;
+			}
+			if (types.includes("administrative_area_level_1")) {
+				state = component.short_name;
+			}
+			if (types.includes("postal_code")) {
+				zip = component.long_name;
+			}
+		});
+		const street = `${streetNumber} ${streetName}`.trim();
+		setAddressDetails({ street, city, state, zip });
+		setInputValue(fullAddress ?? ""); // Set the input value to the full street address
+	};
 
-const inputStyles = "block mx-full mb-[10px] w-full hx-full h-full py-[8px] lg:py-[12px] px-[8px] lg:px-[16px] bg-transparent text-text border-solid border-2 focus:border-[3px] border-secondary rounded-[12px] focus:shadow-input-shadow outline-none xl:placeholder:text-[16px] placeholder:text-secondary-placeholder placeholder-opacity-50";
+	const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+		console.log(inputValue);
+		setInputValue(e.target.value);
+		setAddressDetails({ ...addressDetails, street: e.target.value });
+		handleCustomChange("address", addressDetails);
+		// setAddressDetails({ ...addressDetails, street: inputValue });
+	};
 
-  return (
-    <div className="w-full">
-      <div className="flex flex-col gap-5">
-        <div className="flex gap-5 flex-col lg:flex-row min-w-[280px]">
-          <div className="md:w-full lg:w-1/2 md:h-[48px]">
-            <Autocomplete
-              apiKey="AIzaSyA5V_Sg1fWA7QzML7JwdQmqu-wMSOomO0g"
-              onPlaceSelected={handlePlaceSelected}
-              options={{
-                types: ['address'],
-                componentRestrictions: { country: 'us' },
-              }}
-              className={inputStyles}
-              placeholder="Address*"
-              value={inputValue}
-              // onChange={(e: ChangeEvent<HTMLInputElement>) => handleCustomChange("address", e.target.value)}
-              onChange={handleInputChange}
-              name="address"
-            />
-            {errors.address && (
-              <p className="error" role="alert">
-                {errors.address.message}
-              </p>
-            )}
-          </div>
-          {/* <div className="md:w-full lg:w-1/2 md:h-[48px]">
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		// setAddressDetails({ ...addressDetails, street: inputValue });
+		handleCustomChange("address", addressDetails);
+	};
+
+	const inputStyles =
+		"block mx-full mb-[10px] w-full hx-full h-full py-[8px] lg:py-[12px] px-[8px] lg:px-[16px] bg-transparent text-text border-solid border-2 focus:border-[3px] border-secondary rounded-[12px] focus:shadow-input-shadow outline-none xl:placeholder:text-[16px] placeholder:text-secondary-placeholder placeholder-opacity-50";
+
+	return (
+		<div className="w-full">
+			<form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+				<div className="flex gap-5 flex-col lg:flex-row min-w-[280px]">
+					<div className="md:w-full lg:w-1/2 md:h-[48px]">
+						<Autocomplete
+							apiKey="AIzaSyA5V_Sg1fWA7QzML7JwdQmqu-wMSOomO0g"
+							onPlaceSelected={handlePlaceSelected}
+							options={{
+								types: ["address"],
+								componentRestrictions: { country: "us" },
+							}}
+							className={inputStyles}
+							placeholder="Address*"
+							value={inputValue}
+							// onChange={(e: ChangeEvent<HTMLInputElement>) => handleCustomChange("address", e.target.value)}
+							onChange={handleInputChange}
+							name="address"
+						/>
+						{errors.address && (
+							<p className="error" role="alert">
+								{errors.address.message}
+							</p>
+						)}
+					</div>
+					{/* <div className="md:w-full lg:w-1/2 md:h-[48px]">
             <Input
               type="text"
               style="form-input"
@@ -131,83 +138,86 @@ const inputStyles = "block mx-full mb-[10px] w-full hx-full h-full py-[8px] lg:p
               </p>
             )}
           </div> */}
-        </div>
-        <div className="flex gap-5 flex-col md:flex-row">
-          <div className="md:w-3/4 md:h-[48px] gap-5">
-            <Input
-              type="text"
-              style="form-input"
-              placeholder="City*"
-              // onChange={handleInputChange}
-              {...register("city", {
-                required: "City is required",
-                onChange: (e) => {
-                  // handleInputChange(e);
-                  handleCustomChange("city", addressDetails.city);
-                }
-              })}
-              // onChange={handleInputChange}
-              value={addressDetails.city}
-              name="city"
-            />
-            {errors.city && (
-              <p className="error" role="alert">
-                {errors.city.message}
-              </p>
-            )}
-          </div>
-          <div className="md:w-full md:h-[48px] gap-5">
-            <Input
-              type="text"
-              style="form-input"
-              placeholder="State*"
-              // onChange={handleInputChange}
-              // {...register("state", {
-              //   required: "State is required",
-              //   onChange: (e) => {
-              //     handleInputChange(e);
-              //   }
-              // })}
-              // onChange={handleInputChange}
-              value={addressDetails.state}
-              name="state"
-            />
-            {errors.city && (
-              <p className="error" role="alert">
-                {errors.city.message}
-              </p>
-            )}
-          </div>
-          <div className="md:w-1/4 md:h-[48px]">
-            <Input
-              type="text"
-              style="form-input"
-              placeholder="Zip Code*"
-              value={addressDetails.zip}
-              {...register("zipCode", {
-                required: "Zip Code is required",
-                minLength: { value: 5, message: "Zip Code must be 5 digits" },
-                maxLength: { value: 5, message: "Zip Code must be 5 digits" },
-                pattern: {
-                  value: /^[0-9]+$/,
-                  message: "Zip Code must be numeric and 5 digits",
-                },
-                onChange: (e) => {
-                  // handleInputChange(e);
-                  handleCustomChange("zip", e.target.value);
-                },
-              })}
-            />
-            {errors.zipCode && (
-              <p className="text-red-500" role="alert">
-                {errors.zipCode.message}
-              </p>
-            )}
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+				</div>
+				<div className="flex gap-5 flex-col md:flex-row">
+					<div className="md:w-3/4 md:h-[48px] gap-5">
+						<Input
+							type="text"
+							style="form-input"
+							placeholder="City*"
+							// onChange={handleInputChange}
+							{...register("city", {
+								required: "City is required",
+								onChange: (e) => {
+									// handleInputChange(e);
+									handleCustomChange("city", addressDetails.city);
+								},
+							})}
+							// onChange={handleInputChange}
+							value={addressDetails.city}
+							name="city"
+						/>
+						{errors.city && (
+							<p className="error" role="alert">
+								{errors.city.message}
+							</p>
+						)}
+					</div>
+					<div className="md:w-full md:h-[48px] gap-5">
+						<Input
+							type="text"
+							style="form-input"
+							placeholder="State*"
+							// onChange={handleInputChange}
+							// {...register("state", {
+							//   required: "State is required",
+							//   onChange: (e) => {
+							//     handleInputChange(e);
+							//   }
+							// })}
+							// onChange={handleInputChange}
+							value={addressDetails.state}
+							name="state"
+						/>
+						{errors.city && (
+							<p className="error" role="alert">
+								{errors.city.message}
+							</p>
+						)}
+					</div>
+					<div className="md:w-1/4 md:h-[48px]">
+						<Input
+							type="text"
+							style="form-input"
+							placeholder="Zip Code*"
+							value={addressDetails.zip}
+							{...register("zipCode", {
+								required: "Zip Code is required",
+								minLength: { value: 5, message: "Zip Code must be 5 digits" },
+								maxLength: { value: 5, message: "Zip Code must be 5 digits" },
+								pattern: {
+									value: /^[0-9]+$/,
+									message: "Zip Code must be numeric and 5 digits",
+								},
+								onChange: (e) => {
+									// handleInputChange(e);
+									handleCustomChange("zip", e.target.value);
+								},
+							})}
+						/>
+						{errors.zipCode && (
+							<p className="text-red-500" role="alert">
+								{errors.zipCode.message}
+							</p>
+						)}
+					</div>
+				</div>
+				<Button style="send" type="submit">
+					Submit
+				</Button>
+			</form>
+		</div>
+	);
 };
 
 export default AddressForm;
