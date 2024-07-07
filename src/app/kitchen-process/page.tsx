d96158edba848132c60a97b42c2f8a46ff51b96d
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-07-07 14:50:29 +0300

    footer is bottom

--- src/app/kitchen-process/page.tsx
+++ src/app/kitchen-process/page.tsx
@@ -1,15 +1,13 @@
-"use client";
+'use client';
 
-import KitchenServices from "@/components/Kitchen";
-import { FooterImg } from "@/global/images";
+import KitchenServices from '@/components/Kitchen';
 
 export interface PageProps {}
 
 export default function Page({}: PageProps) {
-  return (
-    <div className=" p-5 md:p-7 lg:p-[42px] xl:p-20">
-      <KitchenServices />
-      <FooterImg />
-    </div>
-  );
+ return (
+  <div className=" p-5 md:p-7 lg:p-[42px] xl:p-20">
+   <KitchenServices />
+  </div>
+ );
 }
--- src/components/FooterDesctop.tsx
+++ src/components/FooterDesctop.tsx
@@ -1,25 +1,25 @@
-"use client";
+'use client';
 
-import Link from "next/link";
-import FooterMainNav from "./Navigation/FooterNav";
-import { FOOTER_NAV } from "@/global/navigation";
+import Link from 'next/link';
+import FooterMainNav from './Navigation/FooterNav';
+import {FOOTER_NAV} from '@/global/navigation';
 // import { FooterImg } from "@/global/images";
 
 export default function FooterDesctop() {
-  return (
-    <footer className="lg:absolute lg:inset-x-0 lg:top-auto lg:left-50 lg:z-100 lg:left-1/2 transform lg:-translate-x-1/2 hidden lg:blok bg-secondary lg:w-[1000px] h-[100px] lg:flex lg:flex-col lg:justify-end  py-4">
-      <div className="lg:w-[1000px] xl:w-[1200px]  ml-auto mr-auto">
-        <FooterMainNav links={FOOTER_NAV} />
-      </div>
-      <div className="flex flex-col justify-center items-center">
-        <div className=" w-[1000px] h-[1px] bg-gradient-to-r from-transparent via-main to-transparent mb-2 mt-2"></div>
-        <p className=" text-white lg:text-main text-[8px] md:text-[10px] lg:text-[14px] font-light">
-          All materials on this website are protected by Copyright ©2024
-          <span className="">
-            <Link href="/"> Lobsters Co.</Link>
-          </span>
-        </p>
-      </div>
-    </footer>
-  );
+ return (
+  <footer className="hidden backdrop-opacity-10 backdrop-invert bg-white/30 w-full h-[100px] lg:flex lg:flex-col lg:justify-end py-4 absolute bottom-0 z-20 right-0">
+   <div className="lg:w-[1000px] xl:w-[1200px]  ml-auto mr-auto">
+    <FooterMainNav links={FOOTER_NAV} />
+   </div>
+   <div className="flex flex-col justify-center items-center">
+    <div className=" w-[1000px] h-[1px] bg-gradient-to-r from-transparent via-main to-transparent mb-2 mt-2"></div>
+    <p className=" text-white lg:text-main text-[8px] md:text-[10px] lg:text-[14px] font-light">
+     All materials on this website are protected by Copyright ©2024
+     <span className="">
+      <Link href="/"> Lobsters Co.</Link>
+     </span>
+    </p>
+   </div>
+  </footer>
+ );
 }
