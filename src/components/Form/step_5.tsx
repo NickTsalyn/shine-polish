a305b4ac98b29c0980a0a692a06c63aff9b281ff
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-07-14 12:20:45 +0300

    update sltyling step 5

--- src/components/Form/step_5.tsx
+++ src/components/Form/step_5.tsx
@@ -3,8 +3,6 @@ import Input from '../UI/Input';
 import Textarea from '../UI/Textarea';
 
 import useFormStorage from '@/hooks/formStorage';
-import Image from 'next/image';
-import tellAboutImg from '../../../public/images/tell-about-discount.png';
 import {BannerImg, DiscontImg} from '../../global/images';
 
 const Step5 = () => {
@@ -19,12 +17,12 @@ const Step5 = () => {
  return (
   <div className=" py-8 flex flex-col gap-6 lg:flex-row lg:flex-wrap justify-between">
    {/* question */}
-   <div className=" flex flex-col gap-6 lg:w-[580px] mb-[40px]">
+   <div className=" flex flex-col gap-6 lg:min-w-[600px] mb-[40px] lg:mb-0 xl:w-[900px]">
     <h2 className=" text-2xl md:text-4xl font-medium">Have you any question?</h2>
     <p className=" body text-subtext ">
      If you have a question about our company, ask it in the next field. We’ll answer to you on email{' '}
     </p>
-    <div className="h-56">
+    <div className="h-48">
      <Textarea
       name="question"
       value={form.question as string}
@@ -35,10 +33,10 @@ const Step5 = () => {
    </div>
    {/* discount */}
    <form className=" flex flex-col justify-center lg:justify-start gap-4 mb-[40px]">
-    <h2 className="text-2xl md:text-4xl font-medium mb-5">Discount code</h2>
+    <h2 className="text-2xl md:text-4xl font-medium lg:mb-5">Discount code</h2>
 
-    <div className="flex  gap-4 lg:flex-col lg:w-[500px]">
-     <div className="md:w-3/4 lg:w-full md:h-[60px]">
+    <div className="md:flex  gap-4 lg:flex-col lg:w-[500px]">
+     <div className="w-full md:w-3/4 lg:w-full h-[60px] mb-5">
       <Input
        style="form-input"
        type="text"
@@ -59,12 +57,12 @@ const Step5 = () => {
     </div>
    </form>
    {/* tips */}
-   <div className="relative flex flex-col md:flex-row  md:w-full md:justify-between lg:flex-row   lg:items-end lg:w-[580px] lg:gap-8 mb-[40px] lg:mb-0">
-    <div className=" hidden md:flex lg:w-[500px] absolute left-0 bottom-0  ">
+   <div className="lg:relative flex flex-col md:flex-row  md:w-full md:justify-between lg:flex-row lg:items-end lg:min-w-[600px] lg:max-w-[800px] lg:gap-8 mb-[40px] lg:mb-0 xl:w-[900px]">
+    <div className=" hidden md:flex lg:w-[500px] lg:absolute lg:left-0 lg:bottom-0  ">
      <DiscontImg />
     </div>
-    <div className="flex flex-col justify-end gap-12 md:w-[480px] lg:w-[300px] absolute right-0 bottom-[6px]">
-     <p className=" text-text body md:text-2xl md:text-right   md:text-accent">
+    <div className="flex flex-col justify-end gap-5 md:w-[480px] lg:w-[300px] lg:absolute lg:right-0 lg:bottom-[6px] xl:w-[500px]">
+     <p className=" body md:text-2xl md:text-right  text-accent">
       Tips are not expected but always appreciated <br />
       by our cleaners
      </p>
@@ -81,7 +79,7 @@ const Step5 = () => {
    </div>
    {/* banner */}
    <div className=" lg:flex md:flex-col lg:w-[500px]">
-    <h2 className="text-center text-rose-900 text-4xl font-bold mb-5">Tell about us your friends and get discount!</h2>
+    <h2 className="text-center text-rose-900 text-4xl mb-5">Tell about us your friends and get discount!</h2>
     <div className=" flex justify-center">
      <BannerImg />
     </div>
