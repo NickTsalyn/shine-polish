5f61acb4daf07ee06ee87f9b84431a13bd38b00e
NickTsalyn <129570585+NickTsalyn@users.noreply.github.com>
2024-07-26 23:31:13 -0400

    api requests for formula calc

--- formula.ts
+++ formula.ts
@@ -1,55 +1,62 @@
+import axios from "axios";
+
+
 const base = 100;
 const coff = 0.25;
 const bathPrice = 30;
 
-export const discount = [
-	{ name: "One-time service", value: 1 },
-	{ name: "Every week", value: 0.8 },
-	{ name: "Every 2 weeks", value: 0.85 },
-	{ name: "Every 4 weeks", value: 0.9 },
-];
-
-export const area = [
-	{ name: "Midtown", value: 1 },
-	{ name: "Buckhead", value: 1 },
-	{ name: "Sandy Springs", value: 1 },
-	{ name: "Smyrna", value: 1.15 },
-	{ name: "Dunwoody", value: 1.15 },
-	{ name: "Roswell", value: 1.15 },
-	{ name: "Decatur", value: 1.15 },
-	{ name: "Alpharetta", value: 1.25 },
-	{ name: "Johns Creek", value: 1.25 },
-	{ name: "Marietta", value: 1.25 },
-	{ name: "Downtown", value: 1.25 },
-];
-
-// export const ServicesOptions = [
-// 	{ value: "Basic Cleaning", label: "Basic Cleaning" },
-// 	{ value: "Deep Cleaning", label: "Deep Cleaning" },
-// 	{ value: "Move In/Move Out", label: "Move In/Move Out" },
-// 	{ value: "Post Constraction", label: "Post Constraction" },
+// export const discount = [
+// 	{ name: "One-time service", value: 1 },
+// 	{ name: "Every week", value: 0.8 },
+// 	{ name: "Every 2 weeks", value: 0.85 },
+// 	{ name: "Every 4 weeks", value: 0.9 },
 // ];
 
-export const serviceOption = [
-	{ name: "Basic Cleaning", value: 1 },
-	{ name: "Deep Cleaning", value: 3 },
-	{ name: "Move In/Move Out", value: 2 },
-	{ name: "Post Constraction", value: 2.5 },
-]
+// export const area = [
+// 	{ name: "Midtown", value: 1 },
+// 	{ name: "Buckhead", value: 1 },
+// 	{ name: "Sandy Springs", value: 1 },
+// 	{ name: "Smyrna", value: 1.15 },
+// 	{ name: "Dunwoody", value: 1.15 },
+// 	{ name: "Roswell", value: 1.15 },
+// 	{ name: "Decatur", value: 1.15 },
+// 	{ name: "Alpharetta", value: 1.25 },
+// 	{ name: "Johns Creek", value: 1.25 },
+// 	{ name: "Marietta", value: 1.25 },
+// 	{ name: "Downtown", value: 1.25 },
+// ];
 
-export const extrasOption = [
-	{name: "Organizing", value: 30},
-	{name: "Load of Laundry", value: 30},
-	{name: "Inside Fridge", value: 50},
-	{name: "Inside Oven", value: 50},
-	{name: "Inside Windows", value: 150},
-	{name: "Unload dishwasher", value: 15},
-	{name: "Blinds", value: 15},
-	{name: "Ironing", value: 30},
-	{name: "Walls", value: 40},
-	{name: "Basement", value: 50},
-]
-export const getPrice = (
+
+// export const serviceOption = [
+// 	{ name: "Basic Cleaning", value: 1 },
+// 	{ name: "Deep Cleaning", value: 3 },
+// 	{ name: "Move In/Move Out", value: 2 },
+// 	{ name: "Post Constraction", value: 2.5 },
+// ]
+
+// export const extrasOption = [
+// 	{name: "Organizing", value: 30},
+// 	{name: "Load of Laundry", value: 30},
+// 	{name: "Inside Fridge", value: 50},
+// 	{name: "Inside Oven", value: 50},
+// 	{name: "Inside Windows", value: 150},
+// 	{name: "Unload dishwasher", value: 15},
+// 	{name: "Blinds", value: 15},
+// 	{name: "Ironing", value: 30},
+// 	{name: "Walls", value: 40},
+// 	{name: "Basement", value: 50},
+// ]
+
+const getData = async() => {
+	try {
+		const response = await axios.get('https://shine-polish-server.onrender.com/bookings/options')
+		return response.data
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+export const getPrice = async (
 	bedrooms: number,
 	bathrooms: number,
 	selectedArea?: string | number,
@@ -58,10 +65,22 @@ export const getPrice = (
 	selectedExtras?: string[]
 ) => {
 
-	const areaCoefficient = area.find((area) => area.name === selectedArea)?.value || 1;
-	const discountValue = discount.find((discount) => discount.name === discountType)?.value || 1;
-	const cleaningValue = serviceOption.find((service) => service.name === selectedService)?.value || 1;
-	const extraValue = extrasOption.reduce((acc, item) => {
+	const {areaOptions, discountOptions, serviceOptions, extrasOptions} = await getData()
+
+	// const areaCoefficient = area.find((area) => area.name === selectedArea)?.value || 1;
+	// const discountValue = discount.find((discount) => discount.name === discountType)?.value || 1;
+	// const cleaningValue = serviceOption.find((service) => service.name === selectedService)?.value || 1;
+	// const extraValue = extrasOption.reduce((acc, item) => {
+	// 	if (selectedExtras?.includes(item.name)) {
+	// 		return acc + item.value
+	// 	} else {
+	// 		return acc
+	// 	}
+	// }, 0)
+	const areaCoefficient = areaOptions.find((area: any) => area.name === selectedArea)?.value || 1;
+	const discountValue = discountOptions.find((discount: any) => discount.name === discountType)?.value || 1;
+	const cleaningValue = serviceOptions.find((service: any) => service.name === selectedService)?.value || 1;
+	const extraValue = extrasOptions.reduce((acc: number, item: any) => {
 		if (selectedExtras?.includes(item.name)) {
 			return acc + item.value
 		} else {
--- src/components/Form/step_6.tsx
+++ src/components/Form/step_6.tsx
@@ -1,20 +1,40 @@
 import useFormStorage from "@/hooks/formStorage";
 // import { getPrice, serviceOption } from "../../../formula";
 import { useEffect, useState } from "react";
-import { area, discount, serviceOption, extrasOption ,getPrice} from "../../../formula";
+// import { area, discount, serviceOption, extrasOption ,getPrice} from "../../../formula";
 
 import { loadStripe } from "@stripe/stripe-js";
+import axios from "axios";
+import getPrice from "../../../formula";
+// import { extrasOption } from '../../../.next/static/chunks/app/booking/[step]/page';
 
 const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY as string);
+
+const getData = async () => {
+	try {
+		const response = await axios.get("https://shine-polish-server.onrender.com/bookings/options");
+		return response.data;
+	} catch (error) {
+		console.log(error);
+	}
+};
 
 const Step6 = () => {
 	const { form } = useFormStorage({});
 	const [subTotal, setSubTotal] = useState(0);
 	const [total, setTotal] = useState(0);
+	const [data, setData] = useState<{
+		areaOptions: { name: string; value: number }[];
+		discountOptions: { name: string; value: number }[];
+		serviceOptions: { name: string; value: number }[];
+		extrasOptions: { name: string; value: number }[];
+		base: number,
+		coff: number,
+		bathPrice: number
+	} | null>(null);
 
 	const [loading, setLoading] = useState(false);
-  // const origin = window.location.origin; // Отримати поточний URL звідки виконується код
-  
+	// const origin = window.location.origin; // Отримати поточний URL звідки виконується код
 
 	useEffect(() => {
 		if (Object.keys(form).length === 0) {
@@ -25,54 +45,80 @@ const Step6 = () => {
 			return;
 		}
 
+		// const { bedroom, bathroom, areas, frequency, services, extras } = form;
+
+		// const areaCoefficient = area.find((area) => area.name === areas)?.value || 1;
+		// const discountValue = discount.find((discount) => discount.name === frequency)?.value || 1;
+		// const cleaningValue = serviceOption.find((service) => service.name === services)?.value || 1;
+		// const extraValue = extrasOption.reduce((acc, item) => {
+		// 	return (extras as string[]).includes(item.name) ? acc + item.value : acc;
+		// }, 0);
+
+		// const calculatedPrice = getPrice(Number(bedroom), Number(bathroom)) * areaCoefficient * discountValue  * cleaningValue + extraValue
+		// setSubTotal(calculatedPrice);
+
+		// const totalPrice = calculatedPrice * 1.06;
+		// setTotal(totalPrice);
+
+		const getResult = async () => {
+			// const { areaOptions, discountOptions, serviceOptions, extrasOptions } = await getData();
+			const result = await axios.get("https://shine-polish-server.onrender.com/bookings/options")
+			console.log(result.data)
+			console.log(result.data.base)
+			setData(result.data)
+			console.log(data?.base)
+		};
+		getResult();
+
 		const { bedroom, bathroom, areas, frequency, services, extras } = form;
+		console.log(form)
 
-		const areaCoefficient = area.find((area) => area.name === areas)?.value || 1;
-		const discountValue = discount.find((discount) => discount.name === frequency)?.value || 1;
-		const cleaningValue = serviceOption.find((service) => service.name === services)?.value || 1;
-		const extraValue = extrasOption.reduce((acc, item) => {
-			return (extras as string[]).includes(item.name) ? acc + item.value : acc;
-		}, 0);
-	
-		const calculatedPrice = getPrice(Number(bedroom), Number(bathroom)) * areaCoefficient * discountValue  * cleaningValue + extraValue
-		setSubTotal(calculatedPrice);
-		
-		const totalPrice = calculatedPrice * 1.06;
-		setTotal(totalPrice);
+		// const areaCoefficient = data.areasOptions.find((area: any) => area.name === areas)?.value || 1;
+		// const discountValue = discountOptions.find((discount: any) => discount.name === frequency)?.value || 1;
+		// const cleaningValue = serviceOptions.find((service: any) => service.name === services)?.value || 1;
+		// const extraValue = extrasOptions.reduce((acc: number, item: any) => {
+		// 	return (extras as string[]).includes(item.name) ? acc + item.value : acc;
+		// }, 0);
 
+		// const calculatedPrice =
+		// 	( getPrice(Number(bedroom), Number(bathroom))) * areaCoefficient * discountValue * cleaningValue +
+		// 	extraValue;
+		// setSubTotal(calculatedPrice);
+
+		// const totalPrice = calculatedPrice * 1.06;
+		// setTotal(totalPrice);
 	}, [form, subTotal, total]);
-
 
 	const handleCheckout = async () => {
 		setLoading(true);
-	
+
 		// Отримайте остаточну ціну з вашої форми букінгу
 		const price = total * 100; // Наприклад, 50.00 USD
-	
-		const response = await fetch('/api/create-checkout-session', {
-		  method: 'POST',
-		  headers: {
-			'Content-Type': 'application/json',
-		  },
-		  body: JSON.stringify({ price }), // ціна у центах
+
+		const response = await fetch("/api/create-checkout-session", {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({ price }), // ціна у центах
 		});
-	
+
 		const session = await response.json();
-	
+
 		const stripe = await stripePromise;
-	
+
 		if (!stripe) {
-		  console.error('Stripe has not been initialized');
-		  setLoading(false);
-		  return;
+			console.error("Stripe has not been initialized");
+			setLoading(false);
+			return;
 		}
 		const { error } = await stripe.redirectToCheckout({ sessionId: session.id });
-	
+
 		if (error) {
-		  console.error('Error redirecting to checkout:', error);
-		  setLoading(false);
+			console.error("Error redirecting to checkout:", error);
+			setLoading(false);
 		}
-	  };
+	};
 
 	return (
 		<div className="p-4 md:p-6 lg:p-9 flex flex-col gap-5">
@@ -84,7 +130,7 @@ const Step6 = () => {
 			</div>
 			<ul className="list-disc ml-6 flex flex-col gap-0.5">
 				{Object.entries(form).map(([key, value]) => {
-					if (['bedroom', 'bathroom', 'areas', 'frequency', 'services'].includes(key) && value !== "") {
+					if (["bedroom", "bathroom", "areas", "frequency", "services"].includes(key) && value !== "") {
 						return (
 							<li key={key} className="text-base ">
 								<span>{key === "bathroom" || key === "bedroom" ? `${value} ${key}(s)` : value}</span>
@@ -93,18 +139,18 @@ const Step6 = () => {
 					}
 				})}
 			</ul>
-			{Array.isArray(form.extras)
-					? <>
-					<p>Extras</p><ul className="list-disc ml-6 flex flex-col gap-0.5">
-					{form.extras.map((extra, index) => (
-						<li key={index} className="text-base">
-							{extra}
-						</li>
-					))}
-
-				</ul>
+			{Array.isArray(form.extras) ? (
+				<>
+					<p>Extras</p>
+					<ul className="list-disc ml-6 flex flex-col gap-0.5">
+						{form.extras.map((extra, index) => (
+							<li key={index} className="text-base">
+								{extra}
+							</li>
+						))}
+					</ul>
 				</>
-			: null}
+			) : null}
 			<div className="flex flex-col gap-2 ">
 				<div className=" flex justify-between text-xl">
 					<span>SUB-TOTAL</span>
