9e07b0ecf27112515ba6853805a765a18b6346b2
MaksymGS <130307586+MaksymGS@users.noreply.github.com>
2024-08-14 17:43:45 +0300

    add list of last bookings

--- src/components/ClientBookings.tsx
+++ src/components/ClientBookings.tsx
@@ -24,11 +24,10 @@ export default function ClientBookings({
     queryKey: ["client-bookings", ownerId],
     queryFn: () => fetchClientBookings(ownerId),
   });
-  // const { _id, name, surname } = bookings;
+  
   const bookings = query.data;
   console.log(query.data);
-  // console.log(bookings[0].createdAt);
-
+ 
   if (query.isError) {
     // console.log(query.error.message);
   }
@@ -37,7 +36,7 @@ export default function ClientBookings({
     return <span>Loading...</span>;
   }
   if (query.isSuccess && !bookings.length) {
-    return <p>You have no bookings yet</p>;
+    return <p>You do not have any bookings yet</p>;
   }
   const {
     name,
@@ -53,7 +52,7 @@ export default function ClientBookings({
           Hello, {name} {surname}
         </h2>
 
-        <div className="w-[422px] shadow-main-shadow rounded-xl py-5 pl-16 pr-10 body flex flex-col gap-y-4">
+        <div className="w-[422px] shadow-main-shadow rounded-xl py-5 pl-16 pr-10 body flex flex-col gap-y-4 mb-3">
           <div className="hidden md:block text-[28px] lg:text-[32px] relative ">
             {name} {surname}
             <div className="absolute -top-0 -left-12">
@@ -77,6 +76,20 @@ export default function ClientBookings({
               <EmailIcon />
             </div>
           </div>
+        </div>
+
+        <div className="w-[220px] shadow-main-shadow rounded-xl p-4">
+          <h3 className="text-[20px] mb-5 text-accent text-center">
+            Your bookings
+          </h3>
+          <ul className="flex flex-col gap-y-1">
+            {bookings.map((booking: any, index: number) => (
+              <li className="flex flex-col items-center text-[20px]" key={index}>
+                {booking.createdAt.slice(0, 10).split("-").reverse().join("/")}
+                <div className="w-[187px] h-0.5 bg-gray-300"></div>
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     )
