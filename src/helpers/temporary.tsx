41627aa700eddf693d3310be948d6c49c5197535
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-09-04 00:33:03 +0300

    changed step_3 to first logic wiyhout refactoring, add some changes to styled

--- src/helpers/temporary.tsx
+++ src/helpers/temporary.tsx
@@ -2,10 +2,10 @@
 
 import React, {useEffect} from "react";
 import useFormStorage from "@/hooks/formStorage";
-import BasicSelect from "../UI/Select";
-import Textarea from "../UI/Textarea";
+import BasicSelect from "../components/UI/Select";
+import Textarea from "../components/UI/Textarea";
 import {aboutUs, homeAccess} from "@/data/booking-form/stepsData";
-import Input from "../UI/Input";
+import Input from "../components/UI/Input";
 import CheckBoxOutlineBlankIcon from "@mui/icons-material/CheckBoxOutlineBlank";
 import CheckBoxIcon from "@mui/icons-material/CheckBox";
 import {IMaskInput} from "react-imask";
@@ -38,14 +38,14 @@ const Step3: React.FC<StepProps> = ({control, setStepCompleted}) => {
 
  return (
   <div className="py-4 md:py-6 lg:py-9 lg:min-h-[600px] xl:h-[1000px] lg:justify-between">
-   <div className="mb-10 xl:mb-20">
+   <div className="mb-10">
     <h2 className="mb-4 md:mb-5 lg:mb-[26px] text-2xl md:text-4xl font-medium md:font-normal">Contact information</h2>
     <p className="text-subtext md:text-2xl mb-6 md:mb-8 lg:mb-5">
      This information will be used to contact you about service
     </p>
-    <div className="flex flex-col md:flex-row md:flex-wrap lg:flex-nowrap gap-4 lg:gap-6 mb-5 md:mb-6 ">
-     {/* Name */}
-     <div className="w-full md:w-[calc(50%-12px)] lg:w-2/4 relative">
+    <div className="flex flex-col lg:flex-row  gap-4 lg:gap-6 lg:flex-nowrap mb-5 md:mb-6  ">
+     {/* <div className="md:flex md:flex-row  md:gap-x-4 "> */}
+     <div className="w-full md:w-full md:h-[48px] lg:w-2/4 relative">
       <Controller
        name="name"
        control={control}
@@ -73,8 +73,7 @@ const Step3: React.FC<StepProps> = ({control, setStepCompleted}) => {
        )}
       />
      </div>
-     {/* Surname */}
-     <div className="w-full md:w-[calc(50%-12px)] lg:w-2/4 md:h-[48px] relative">
+     <div className="w-full md:w-3/4 lg:w-2/4 md:h-[48px] relative">
       <Controller
        name="surname"
        control={control}
@@ -102,8 +101,9 @@ const Step3: React.FC<StepProps> = ({control, setStepCompleted}) => {
        )}
       />
      </div>
-     {/* Email */}
-     <div className="w-full md:w-[calc(50%-12px)] lg:w-2/5 md:h-[48px] relative">
+     {/* </div> */}
+     {/* <div className="md:flex md:flex-row  md:gap-x-4 "> */}
+     <div className="w-full md:w-2/4 lg:w-3/5 md:h-[48px] relative">
       <Controller
        name="email"
        control={control}
@@ -131,8 +131,7 @@ const Step3: React.FC<StepProps> = ({control, setStepCompleted}) => {
        )}
       />
      </div>
-     {/* Phone */}
-     <div className="w-full md:w-[calc(50%-12px)] lg:w-2/5 md:h-[48px] relative">
+     <div className="w-full md:w-1/4 lg:w-2/5 md:h-[48px] relative">
       <Controller
        name="phone"
        control={control}
@@ -160,14 +159,16 @@ const Step3: React.FC<StepProps> = ({control, setStepCompleted}) => {
        )}
       />
      </div>
+     {/* </div> */}
     </div>
+   </div>
 
-    {/* ADDITIONAL INFORMATION */}
+   {/* ADDITIONAL INFORMATION */}
 
-    {/* <div className="flex flex-col lg:w-full "> */}
-    <div className="flex flex-col  lg:w-full  mb-10 lg:mb-[60px] ">
+   <div className="flex flex-col lg:w-full ">
+    <div className="flex flex-col  lg:w-full h-auto lg:relative mb-10 lg:mb-[60px] ">
      <h2 className="mb-6 md:mb-7 lg:mb-5 text-2xl md:text-4xl font-medium md:font-normal">Additional information</h2>
-     <div className=" flex flex-col lg:flex-row lg:flex-nowrap gap-4  md:gap-6  mb-2">
+     <div className=" flex flex-col lg:flex-row lg:flex-nowrap gap-4  md:gap-6 md:h-8">
       <div className="w-full lg:w-4/12  relative">
        <Controller
         name="homeAccess"
@@ -214,22 +215,22 @@ const Step3: React.FC<StepProps> = ({control, setStepCompleted}) => {
         )}
        />
       </div>
-     </div>
-     {/* SEND REMINDERS */}
+      {/* SEND REMINDERS */}
 
-     <div className="flex lg:h-[40px] gap-2 md:gap-5 lg:w-3/12  ">
-      <button
-       type="button"
-       className="flex items-center justify-center p-0 w-4 md:w-6 h-4 md:h-6 rounded"
-       onClick={handleCheckBoxChange}
-      >
-       {form.remindersChecked ? (
-        <CheckBoxIcon className="w-full h-full fill-main" />
-       ) : (
-        <CheckBoxOutlineBlankIcon className="w-full h-full fill-main" />
-       )}
-      </button>
-      <p className="text-secondary text-xs md:text-xl">Send me reminders about my booking via email</p>
+      <div className="flex lg:h-[40px] items-center gap-2 md:gap-5 lg:w-3/12  ">
+       <button
+        type="button"
+        className="flex items-center justify-center p-0 w-4 md:w-6 h-4 md:h-6 rounded"
+        onClick={handleCheckBoxChange}
+       >
+        {form.remindersChecked ? (
+         <CheckBoxIcon className="w-full h-full fill-main" />
+        ) : (
+         <CheckBoxOutlineBlankIcon className="w-full h-full fill-main" />
+        )}
+       </button>
+       <p className="text-secondary text-xs md:text-xl">Send me reminders about my booking via email</p>
+      </div>
      </div>
     </div>
 
