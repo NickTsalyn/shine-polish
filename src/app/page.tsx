03e133aa93be256327897db72f48855c1056e65b
YuliaSilk <yuliashevchyk0506@gmail.com>
2024-09-14 00:19:56 +0300

    add correcttly arrow to the mobile screen, fixed textArea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Hero from "@/components/Home/Hero";
 import SectionEstimate from "@/components/Home/SectionEstimate";
 import BeforeAfter from "@/components/Home/BeforeAfter";
 import dataSlider from "@/data/slider-data.json";
-import { Reviews } from "@/components/Home/Reviews";
+import {Reviews} from "@/components/Home/Reviews";
 import ServiceAreas from "@/components/Home/ServiceAreas";
 import SectionJustAsk from "@/components/Home/JustAskSection";
 import React from "react";
@@ -11,19 +11,21 @@ import LastSectionHome from "@/components/Home/LastSectionHome";
 import dataBackground from "@/data/background-data.json";
 import dataHeroBackground from "@/data/background-hero.json";
 import Arrow from "@/components/Home/Arrow";
+import ArrowMobile from "@/components/Home/ArrrowMobile";
 
 export default function Home() {
-  return (
-    <>
-      <Arrow />
-      <Hero data={dataHeroBackground} />
-      <SectionEstimate />
-      <BeforeAfter data={dataSlider} />
-      <Reviews />
-      <ServiceAreas />
-      <SectionJustAsk />
-      <WorkWithUs />
-      <LastSectionHome data={dataBackground} />
-    </>
-  );
+ return (
+  <>
+   <ArrowMobile />
+   <Arrow />
+   <Hero data={dataHeroBackground} />
+   <SectionEstimate />
+   <BeforeAfter data={dataSlider} />
+   <Reviews />
+   <ServiceAreas />
+   <SectionJustAsk />
+   <WorkWithUs />
+   <LastSectionHome data={dataBackground} />
+  </>
+ );
 }
